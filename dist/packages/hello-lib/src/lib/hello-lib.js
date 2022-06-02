58883c5a4af184741e54b8421397fd0a14731476
licaide <licaide666@sina.com>
2022-06-02 19:41:53 +0800

    qqq

---
+++ dist/packages/hello-lib/src/lib/hello-lib.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloLib = void 0;
+function helloLib() {
+    return 'hello-lib';
+}
+exports.helloLib = helloLib;
+//# sourceMappingURL=hello-lib.js.map
