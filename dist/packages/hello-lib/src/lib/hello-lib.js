d5f274d584693615f2557f2646d4308a402c18a9
licaide <licaide666@sina.com>
2022-06-04 16:10:44 +0800

    fix: add

--- dist/packages/hello-lib/src/lib/hello-lib.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloLib = void 0;
-function helloLib() {
-    return 'hello-lib';
-}
-exports.helloLib = helloLib;
-//# sourceMappingURL=hello-lib.js.map
