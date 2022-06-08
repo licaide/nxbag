e2caa99a87b5f172d79022d8d85b106b5ad8e06a
licaide <licaide666@sina.com>
2022-06-08 13:49:20 +0800

    fix: add lib

--- packages/hello-lib/src/lib/hello-lib.ts
+++ packages/hello-lib/src/lib/hello-lib.ts
@@ -1,5 +1,5 @@
 //import { worldLib } from '../../../world-lib/src/index';
-import { worldLib } from "@nxbag/world-lib";
+import { worldLib } from "@licaide/world-lib";
 export function helloLib(): string {
     return 'hello-lib+108' + worldLib();
 }
